c4733ed5c382fde5ed4eb5144bfb6a39bedf0618
Milo Hyben <milo-hyben@users.noreply.github.com>
2025-04-24 15:57:02 +1000

    Fixing CI deploy & Cleanup (#3)
    * Add deploy.yaml to github actions.
    
    * Add deploy.yaml to github actions.
    
    * Change github action working directory for docker builds.
    
    * Change github action working directory for docker builds.
    
    * Fixing deocker builds.
    
    * Fixing docker builds take 2.
    
    * Fixing docker builds take 3.
    
    * Fixing docker builds take 4.
    
    * Fixing docker builds take 5.
    
    * Fixing docker builds take 6.
    
    * Fixing docker builds take 7.
    
    * Fixing docker builds take 8.
    
    * Fixing docker builds take 9.
    
    * Fixing docker builds take 10.
    
    * Enable debug logs in nginx.
    
    * Enable debug logs in nginx.
    
    * Enable debug logs in nginx.
    
    * Enable debug logs in nginx.
    
    * Added GOOGLE_APPLICATION_CREDENTIALS file to blog image.
    
    * Added GOOGLE_APPLICATION_CREDENTIALS file to blog image, take 2.
    
    * Added GOOGLE_APPLICATION_CREDENTIALS file to blog image, take 3.
    
    * Trying gcsfuse approach.
    
    * Trying gcsfuse approach, take 2.
    
    * Trying gcsfuse approach, take 3.
    
    * Trying gcsfuse approach, take 4.
    
    * Trying gcsfuse approach, take 5.
    
    * Trying gcsfuse approach, take 6.
    
    * Trying gcsfuse approach, take 7.
    
    * Cleanup actions, header, auth part.
    
    * Fixing navigation.

--- src/components/layout.js
+++ src/components/layout.js
@@ -48,8 +48,8 @@ const Banner = styled.div`
 
 const BANNER_CONTENT = (
   <>
-    Help us continue to improve gnomAD by taking 5 minutes to fill out our {/* @ts-expect-error */}
-    <ExternalLink href="http://broad.io/2024_survey">user survey</ExternalLink>.
+    This is <b>DEVELOPMENT</b> version of OurDNA Browser! Please visit released version{' '}
+    <ExternalLink href="https://ourdna.populationgenomics.org.au">here</ExternalLink>.
   </>
 );
 
